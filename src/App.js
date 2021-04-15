2d5409bdf14a7d6e787d0acab54727915b830882
DoDifferent <dodifferent@dodifferent.it>
2021-04-15 16:11:11 +0200

    http requests with axios as props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, {Component } from "react";
+import React, {Component} from "react";
 import './App.css';
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
-class App extends Component{
-    foo = () => 'Have a good time!';
+import axios from "axios";
 
-    render(){
+class App extends Component {
+    state = {
+        users: [],
+        loading: false
+    }
+
+    async componentDidMount() {
+        this.setState({loading: true})
+        const res = await axios.get('https://api.github.com/users');
+        this.setState({users: res.data, loading: false});
+
+        console.log(res.data)
+    }
+
+    render() {
 
         return (
             <div className='App'>
                 <Navbar iconProp='fab fa-github'/>
-                <div className='container'><Users/></div>
+                <div className='container'>
+                    <Users loading={this.state.loading} users={this.state.users}/>
+                </div>
 
             </div>
         );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,35 +2,13 @@ import React, {Component} from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login: 'eenie',
-                avatar_url: 'imahttps://th.bing.com/th/id/OIP.wxcvrOn64c-3AAh9xXU3uAHaC8?pid=ImgDet&rs=1ge',
-                html_url: 'https://github.com/eenie'
-            },
-            {
-                id: '2',
-                login: 'meenie',
-                avatar_url: 'imahttps://th.bing.com/th/id/OIP.wxcvrOn64c-3AAh9xXU3uAHaC8?pid=ImgDet&rs=1ge',
-                html_url: 'https://github.com/meenie'
-            },
-            {
-                id: '3',
-                login: 'manie',
-                avatar_url: 'imahttps://th.bing.com/th/id/OIP.wxcvrOn64c-3AAh9xXU3uAHaC8?pid=ImgDet&rs=1ge',
-                html_url: 'https://github.com/manie'
-            },
 
-        ]
-    }
 
     render() {
 
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                     <UserItem key={user.id} user={user}/>
                 ))}
             </div>
