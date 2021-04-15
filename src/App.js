244cde26aaa6407800c5c7d46239467cf9ecf52f
DoDifferent <dodifferent@dodifferent.it>
2021-04-15 17:04:16 +0200

    Refactor + github creadentials

--- src/App.js
+++ src/App.js
@@ -11,8 +11,14 @@ class App extends Component {
     }
 
     async componentDidMount() {
+        const apiID = process.env.REACT_APP_GITHUB_CLIENT_ID
+        const apiSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
+        const requestString = 'https://api.github.com/users?client_id=' + apiID + '&client_secret=' + apiSecret
+
+        console.log(requestString)
+
         this.setState({loading: true})
-        const res = await axios.get('https://api.github.com/users');
+        const res = await axios.get(requestString);
         this.setState({users: res.data, loading: false});
 
         console.log(res.data)
