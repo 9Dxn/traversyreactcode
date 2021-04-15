9dc7026291195af6d30eb63c0eb587617617a926
DoDifferent <dodifferent@dodifferent.it>
2021-04-15 12:32:37 +0200

    Jsx manipulation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, {Component, Fragment} from "react";
 import './App.css';
 
 class App extends Component{
+    foo = () => 'Have a good time!';
+
     render(){
+        const name = "John Doe";
+        const booler = true;
+        const showName = true;
+
         return (
-            <Fragment>
-                <h1>Herrooo</h1>
-                <h2>Bye byeee</h2>
-            </Fragment>
+            <div className='App'>
+                {!booler ? <h4>booling</h4> :
+                    <h1>Herrooo {!showName && name.toUpperCase()}</h1>
+                }
+                <h2>Bye byeee foo</h2>
+            </div>
         );
     }
 }
