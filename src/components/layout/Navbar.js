56b0d0c6ab6790b57f86050b29d7702b7ae96b91
DoDifferent <dodifferent@dodifferent.it>
2021-04-15 15:53:02 +0200

    Stateless functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,22 @@
-import React, {Component} from "react";
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-    static defaultProps = {
-        title: 'Github finder',
-        iconProp: 'fab fa-github'
-    };
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        iconProp: PropTypes.string.isRequired
-    }
-
-    render() {
+const Navbar = ({iconProp, title}) => {
         return (
             <nav className='navbar bg-primary'>
-                <h1><i className={this.props.iconProp}/>{this.props.title}</h1>
+                <h1><i className={iconProp}/>{title}</h1>
             </nav>
         )
-    }
 }
+
+Navbar.defaultProps = {
+    title: 'Github finder',
+    iconProp: 'fab fa-github'
+};
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    iconProp: PropTypes.string.isRequired
+}
+
+
 
 export default Navbar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,31 +1,27 @@
-import React, {Component} from "react";
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-    state = {
-            id: 'id',
-            login: 'userId',
-            avatar_url: 'imahttps://th.bing.com/th/id/OIP.wxcvrOn64c-3AAh9xXU3uAHaC8?pid=ImgDet&rs=1ge',
-            html_url: 'https://github.com/userId'
-        }
-
-
-    render() {
-        const {login, avatar_url, html_url} = this.props.user;
-        return <div className='card text-center'>
-            <img
-                src={avatar_url}
-                alt=""
-                className='round-img'
-                style={{width : '60px'}}
-            />
-            <h3>{login}</h3>
-            <div>
-                <a href={html_url} className='btn btn-dark btn-sm my-1'>
-                    More
-                </a>
-            </div>
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
+    return <div className='card text-center'>
+        <img
+            src={avatar_url}
+            alt=""
+            className='round-img'
+            style={{width: '60px'}}
+        />
+        <h3>{login}</h3>
+        <div>
+            <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                More
+            </a>
         </div>
-    }
+    </div>
 }
+
+UserItem.propTypes = {
+    login: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired
+}
+
 
 export default UserItem;
